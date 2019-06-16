ea5e1105f853511f58b9a9efe820cb28690be2d8
karmek-k <33288445+karmek-k@users.noreply.github.com>
2019-06-16 14:33:49 +0200

    Add Fetcher and JSONParser

---
+++ zloto/data.py
@@ -0,0 +1,31 @@
+import json
+from datetime import date
+
+import requests
+
+class Fetcher:
+    url = "http://api.nbp.pl/api/cenyzlota"
+    data = b""
+
+    def __init__(self, url):
+        self.fetch_data(url)
+
+    def fetch_data(self, url):
+        request = requests.get(url)
+        if request.status_code == 200:
+            self.data = request.content
+
+
+class JSONParser:
+    def __init__(self, json_data):
+        self.set_data(self.parse(json_data))
+
+    def parse(self, json_data):
+        if json_data == b"":
+            raise ValueError("Empty data")
+        else:
+            return json.loads(json_data)[0]
+    
+    def set_data(self, parsed_json):
+        self.date = date.fromisoformat(parsed_json["data"])
+        self.price = parsed_json["cena"]
