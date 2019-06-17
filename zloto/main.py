9bc40648e4c36e98173ac0d349a6b92886d3e30e
karmek-k <33288445+karmek-k@users.noreply.github.com>
2019-06-17 16:36:07 +0200

    load args from command line

--- zloto/main.py
+++ zloto/main.py
@@ -6,6 +6,19 @@ from zloto.data import Fetcher, JSONParser
 
 locale.setlocale(locale.LC_TIME, ('pl_PL', 'UTF-8'))
 def main():
+    # load args
+    if len(sys.argv) > 2:
+        print("Nieprawidłowa ilość argumentów!")
+        sys.exit(1)
+    elif len(sys.argv) == 2:
+        try:
+            mass = int(sys.argv[1])
+        except ValueError:
+            print("Nieprawidłowy typ argumentu!")
+            sys.exit(1)
+    else:
+        mass = 1
+
     # load data from NBP
     fetch = Fetcher(Fetcher.url)
     if fetch.data == b"":
@@ -16,9 +29,12 @@ def main():
     parser = JSONParser(fetch.data)
 
     # print message
-    message = "W dniu {0} 1g złota próby 1000 kosztował {1} złotych."
-    print(message.format(
-        parser.date.strftime("%d %B %Y"),
-        parser.price
-    ))
+    message = "W dniu {} {}g złota próby 1000 kosztował(y) {} złotych."
+    print(
+        message.format(
+            parser.date.strftime("%d %B %Y"),
+            mass,
+            round(parser.price * mass),
+        )
+    )
     
