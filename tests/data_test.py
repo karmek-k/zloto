ff3c1e8ae719f4a8f56fd8afac5f60dca593a296
karmek-k <33288445+karmek-k@users.noreply.github.com>
2019-06-17 11:15:42 +0200

    add a test

---
+++ tests/data_test.py
@@ -0,0 +1,18 @@
+import unittest
+from datetime import date
+
+from zloto.data import JSONParser
+
+
+class JSONParserTest(unittest.TestCase):
+    def setUp(self):
+        self.json = b'[{"data":"2019-06-17","cena":163.89}]'
+        self.parser = JSONParser(self.json)
+
+    def test_parsing(self):
+        self.assertEqual(self.parser.date, date(2019, 6, 17))
+        self.assertEqual(self.parser.price, 163.89)
+
+
+if __name__ == "__main__":
+    unittest.main()
